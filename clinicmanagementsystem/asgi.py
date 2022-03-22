19b0cf38baf99ed3e69acedab530a968b215ac75
Moses Chawawa <moseschawawa@gmail.com>
2022-03-22 21:30:19 +0200

    Code cleanup

--- clinicmanagementsystem/asgi.py
+++ clinicmanagementsystem/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hospitalmanagement project.
+ASGI config for clinicmanagementsystem project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospitalmanagement.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clinicmanagementsystem.settings')
 
 application = get_asgi_application()
